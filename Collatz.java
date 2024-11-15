d195a312b80bb755b2b7fdcbc166bd43c8b3d73f
University <inonuni@ilahyany-mbp.local>
2024-11-15 19:39:41 +0200

    first try

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,28 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int seedNumByUser = Integer.parseInt(args[0]), numberOfSteps, currentNumber, numberIsOne;
+		String modeOfVerbosity = args[1];
+		for (int i = 1; i < seedNumByUser+1; i++) {
+			currentNumber = i;
+			numberOfSteps = 1;
+			numberIsOne = 0;
+			while (numberIsOne!=1) {
+				if (modeOfVerbosity.equals("v")) {
+					System.out.print(" " + currentNumber);
+				}
+				if (currentNumber % 2 == 0) {
+					currentNumber = currentNumber/2;
+				} else { 
+					currentNumber = currentNumber*3 + 1;
+				}
+				numberOfSteps++;
+				numberIsOne = (currentNumber == 1 ? 1 : numberIsOne);
+			}
+			if (modeOfVerbosity.equals("v")) {
+				System.out.println(" " + currentNumber + " (" + numberOfSteps + ")");
+			}
+		}
+		System.out.println("Every one of the first " + seedNumByUser +" hailstone sequences reached 1.");
 	}
 }
